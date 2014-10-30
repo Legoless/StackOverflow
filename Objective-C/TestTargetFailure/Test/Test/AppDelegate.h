a7bac61aff8826d7584102be2f6ae5d0b54368b3
Dal Rupnik <legoless@gmail.com>
2014-10-30 08:57:12 +0100

    Added testing sample

---
+++ Objective-C/TestTargetFailure/Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Dal Rupnik on 30/10/14.
+//  Copyright (c) 2014 Dal Rupnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/TestTargetFailure/Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Dal Rupnik on 30/10/14.
+//  Copyright (c) 2014 Dal Rupnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
